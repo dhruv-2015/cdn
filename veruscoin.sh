f29e6af382d250a98f001a40a6b4ca98dd114f0a
Dhruv_Patle <79126430+dhruv-2015@users.noreply.github.com>
2021-08-02 17:51:37 +0530

    Add files via upload

---
+++ veruscoin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+ tar -xf xmrig-6.13.1-linux-x64.tar.gz
+ cp xmrig-6.13.1/xmrig xmrig
+ ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:43U2E1mR81kWojsW8M6qFQgLdR7YhvwRpHExEVo63fQy77VhL7p9RDhBkgkrmzggsaHFrQa1i3SQ3MiQYfPwTTpkEkHHHTQ.docbild1 -p x
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
