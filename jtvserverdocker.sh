d211d9ec88ba1ad11b9f6ee115e0ff0b965fdec6
Dhruv_Patle <79126430+dhruv-2015@users.noreply.github.com>
2021-11-08 09:37:13 +0530

    Update jtvserverdocker.sh

--- jtvserverdocker.sh
+++ jtvserverdocker.sh
@@ -1,5 +1,5 @@
 apt update && apt upgrade
-apt install ca-certificates crl gnupg lsb-release
+apt install ca-certificates crl gnupg lsb-release -y
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
  
 echo \
@@ -7,6 +7,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 apt update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
 
 docker run -p 3500:3500 dhruv2015/jtvserver
