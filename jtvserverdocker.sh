aea3e38eb56b5d805d488172d6f5beda2dcd6903
Dhruv_Patle <79126430+dhruv-2015@users.noreply.github.com>
2021-11-08 09:32:37 +0530

    Add files via upload

---
+++ jtvserverdocker.sh
@@ -0,0 +1,12 @@
+apt update && apt upgrade
+apt install ca-certificates crl gnupg lsb-release
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+apt update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+docker run -p 3500:3500 dhruv2015/jtvserver
